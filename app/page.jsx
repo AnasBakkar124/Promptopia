f9f76fcdd465fc6bf7ac0e8aad2872d0c681888a
BakkarAnas <anas.dev+edi@fascom.com>
2024-07-19 19:05:04 +0500

    folder structure

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+// Home Page
+import Feed from '@/components/Feed'
+
+const Home = () => {
+  return (
+   <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">Discover & Share
+    <br className="max-md:hidden"/>
+    <span className="orange_gradient text-center">AI-Powered Prompts</span>
+    </h1>
+    <p className="desc text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem assumenda magnam odit et adipisci voluptatibus unde sed, recusandae id voluptatum commodi quaerat sequi dolore eos pariatur quod delectus dicta sapiente.
+    </p>
+
+
+    {/* Feed */}
+
+   </section>
+  )
+}
+
+export default Home
